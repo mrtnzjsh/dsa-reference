8ce4e4bd675e0232c74d4b82947e4f1fc8f2426f
Josh Martinez <mrtnz.jnmn@gmail.com>
2026-03-05 17:26:49 -0500

    add flake for dev environment

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Reference for data structures and algorithms";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          python313
+        ];
+      };
+    });
+}
